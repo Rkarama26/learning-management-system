dd1820abf351a2099f8c8187b3944b137101a0cc
Rohit <rohit.karma026@gmail.com>
2024-12-19 20:01:49 +0530

    Oauth2 controller , service , impl added

--- src/main/java/com/example/Teacher_portal/service/OAuth2Service.java
+++ src/main/java/com/example/Teacher_portal/service/OAuth2Service.java
@@ -1,2 +1,17 @@
-package com.example.Teacher_portal.service;public class OAuth2Service {
+package com.example.Teacher_portal.service;
+
+import com.example.Teacher_portal.response.OAuthTokenResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+public interface OAuth2Service {
+
+    String getAuthorizationUrl();
+
+    ResponseEntity<OAuthTokenResponse> exchangeAuthorizationCodeForToken(String authorizationCode);
+
+    ResponseEntity<OAuthTokenResponse> refreshAccessToken() throws JsonProcessingException;
 }
+
