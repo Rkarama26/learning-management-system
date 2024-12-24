14f64cb82f807cc106e46e8619b70e913e1f38f3
Rohit <rohit.karma026@gmail.com>
2024-12-24 18:16:10 +0530

    yml/constants

---
+++ src/main/java/com/example/Teacher_portal/constants/CLconstants.java
@@ -0,0 +1,14 @@
+package com.example.Teacher_portal.constants;
+
+public class CLconstants {
+
+    public static final String REDIRECT_URI = "";
+    public static final String CLIENT_ID = "";
+    public static final String SCOPE = "";
+    public static final String AUTHORIZATION_URL = "";
+    public static final String CLIENT_SECRET = "";
+    public static final String TOKEN_ENDPOINT = "";
+
+//https://zoom.us/oauth/authorize?response_type=code&client_id=GLirsGgSWi22AJr3tuojA&redirect_uri=https://api.zoom/classroom
+
+}
