5f530dcd8eeeb41d6b084646ac673126a8ee030b
Rohit <rohit.karma026@gmail.com>
2024-12-24 18:19:21 +0530

    gitignore

--- src/main/java/com/example/Teacher_portal/constants/CLconstants.java
+++ src/main/java/com/example/Teacher_portal/constants/CLconstants.java
@@ -2,12 +2,12 @@ package com.example.Teacher_portal.constants;
 
 public class CLconstants {
 
-    public static final String REDIRECT_URI = "";
-    public static final String CLIENT_ID = "";
-    public static final String SCOPE = "";
-    public static final String AUTHORIZATION_URL = "";
-    public static final String CLIENT_SECRET = "";
-    public static final String TOKEN_ENDPOINT = "";
+    public static final String REDIRECT_URI = "https://api.zoom/classroom";
+    public static final String CLIENT_ID = "MGaqEQN9SYmctFcSGNvAZg";
+    public static final String SCOPE = "user:read:zak:admin";
+    public static final String AUTHORIZATION_URL = "https://zoom.us/oauth/authorize";
+    public static final String CLIENT_SECRET = "DRAQF3ORlPaBUHoJsn3BLJLrWFiyox9Y";
+    public static final String TOKEN_ENDPOINT = "https://zoom.us/oauth/token";
 
 //https://zoom.us/oauth/authorize?response_type=code&client_id=GLirsGgSWi22AJr3tuojA&redirect_uri=https://api.zoom/classroom
 
